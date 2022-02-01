0badfbf67122d6856b9b710ae18b2ebe1dfaceb5
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-31 23:01:51 -0500

    improve app settings and add initial routes

--- src/app.js
+++ src/app.js
@@ -9,6 +9,28 @@ const path = require("path");
 const app = express();
 const DEFAULT_PORT = 4000;
 app.set("port", process.env.PORT || DEFAULT_PORT);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// requests
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Hi bro");
+});
+
+/*
+app.get("/", (req, res, next) => {
+
+});
+*/
 
 // server listening
 app.listen(app.get("port"), () => {
